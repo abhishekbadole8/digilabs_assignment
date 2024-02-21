7be809181a8adb08b7f49f9c2184effcc87afc1b
Abhishek Badole <abhishekbadole8@gmail.com>
2024-02-21 11:57:55 +0530

    home route created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,15 @@
+import Header from "@/components/Header";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
-      <body>{children}</body>
+      <body>
+        <div className="dashboard">
+          <Header />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
-import Link from "next/link";
+import styles from "@/styles/dashboard.module.css";
+import Item1 from "@/components/Item1";
+import Item2 from "@/components/Item2";
+import Item3 from "@/components/Item3";
 
-export default function Home() {
+export default function Dashboard() {
   return (
-    <Link href={'/pages/dashboard'}>Go to dashboard</Link>
+    <div className={styles.section}>
+      <div className={styles.gridContainer}>
+        <Item1 />
+
+        <Item2 />
+      </div>
+
+      <Item3 />
+    </div>
   );
 }
